c4dd0874ae4977e9cd1c99b15b01ce592cf52f40
1397648351 <1397648351@qq.com>
2018-09-28 18:29:53 +0800

    1

--- grab.py
+++ grab.py
@@ -8,7 +8,7 @@ sys.setdefaultencoding('utf-8')
 
 class Grab:
     headers = [{
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     }, {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'
     }]
